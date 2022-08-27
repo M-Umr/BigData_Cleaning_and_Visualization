498d34fb749cd52fde45a9d4e015f0c05c6b3c9e
“Muhammad <f178224@nu.edu.pk>
2022-08-28 02:19:05 +0500

    remove error in settings.py and do a little changein html file

--- Django app/bigdata_visualization_app/settings.py
+++ Django app/bigdata_visualization_app/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +142,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
